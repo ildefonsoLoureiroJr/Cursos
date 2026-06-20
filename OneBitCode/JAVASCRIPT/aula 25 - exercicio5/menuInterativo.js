e0674e7c798c8911c4afd67182074bf1531d1630
Ildefonso Campos Loureiro Junior <wiiil.hc@gmail.com>
2026-06-20 11:54:57 -0300

    atualizando curso com arrays
    Atualizando curso com arrays

---
+++ OneBitCode/JAVASCRIPT/aula 25 - exercicio5/menuInterativo.js
@@ -0,0 +1,32 @@
+let opcao = ""
+
+do{
+    opcao = prompt(
+        "Seja bem-vindo(a)\n" +
+        "\nEscolha uma das opções abaixo: " +
+        "\n1. Opção Um" + 
+        "\n2. Opção Dois" + 
+        "\n3. Opção Três" + 
+        "\n4. Opção Quatro" + 
+        "\n5. Encerrar" 
+    )
+
+    switch (opcao) {
+        case "1":
+            alert("Você escolheu a Opção 1")
+            break
+        case "2":
+            alert("Você escolheu a Opção 2")
+            break
+        case "3":
+            alert("Você escolheu a Opção 3")
+            break
+        case "4":
+            alert("Você escolheu a Opção 4")
+            break
+        case "5":
+            alert("Encerrando...")
+            break
+    }
+
+} while (opcao !== "5")
---
+++ OneBitCode/JAVASCRIPT/aula 27 - For/for.js
@@ -0,0 +1,18 @@
+//inicialização ; condição; finalização
+
+/* let indice = 0
+
+for (
+    alert("Inicializando o for");
+    indice <= 10;
+    indice++
+) {
+    alert("Indice = " + indice)
+}
+    */
+
+let nome = "Isaac Pontes"
+
+for (let indice = 0; indice < nome.length; indice++){
+    console.log(nome[indice])
+}
