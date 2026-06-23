9ce69345530dd7c103e3424e4c6c6255e9caf7df
Ildefonso Campos Loureiro Junior <wiiil.hc@gmail.com>
2026-06-23 16:55:33 -0300

    Metodos Js
    atualizando curso, metodos em js

---
+++ OneBitCode/JAVASCRIPT/aula 47 - metodos/metodos.js
@@ -0,0 +1,11 @@
+let pessoa = {
+    nome: "Ildefonso",
+    idade: 39,
+    dizerOla() {
+        console.log("olá mundo! meu nome é " + this.nome)
+    }
+}
+
+console.log(pessoa)
+pessoa.dizerOla()
+
